2182469d63d58011de159f004728db26d3a9c9b6
Daniel Mott <daniel.r.mott@gmail.com>
2024-02-21 17:32:28 -0800

    Functioning game

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const letters = document.querySelector(".guessed-letters");
+const guessedLettersElement = document.querySelector(".guessed-letters");
 const button = document.querySelector(".guess");
 const textInput = document.querySelector("#letter");
 const inProgress = document.querySelector(".word-in-progress");
@@ -24,9 +24,11 @@ button.addEventListener("click", function (e) {
   e.preventDefault();
   message.innerText = "";
   const inputValue = textInput.value;
-  textInput.value = "";
-    const validInput = inputValidator(inputValue);
-  makeGuess(validInput);
+  const validInput = inputValidator(inputValue);
+  if (validInput) {
+  makeGuess(inputValue);
+}
+textInput.value = "";
 });
 
 const inputValidator = function (guess) {
@@ -38,17 +40,50 @@ const inputValidator = function (guess) {
   } else if (!guess.match(acceptedLetter)) {
     message.innerText = "Please enter a  valid letter";
   } else {
-    message.innerText = "Great! Enter another letter";
-  }
   return guess;
+  }
 };
 
 const makeGuess = function (letter) {
-  letter.toUppercase;
+  console.log("makeGuess called with letter", letter);
+  letter = letter.toUpperCase();
   if (guessedLetters.includes(letter)) {
     message.innerText = "You have already guessed that letter";
   } else {
     guessedLetters.push(letter);
+    showGuess();
+    updateWord(guessedLetters);
   }
-  console.log(guessedLetters);
+  //console.log(guessedLetters);
+};
+
+const showGuess = function () {
+  guessedLettersElement.innerHTML = "";
+  for (const letter of guessedLetters) {
+    const li = document.createElement("li");
+    li.innerText = letter;
+    guessedLettersElement.append(li);
+  }
+};
+
+const updateWord = function (guessedLetters) {
+  const wordUpper = word.toUpperCase();
+  const wordArray = wordUpper.split("");
+  const goodGuessArray = [];
+  for (const letter of wordArray) {
+    if (guessedLetters.includes(letter)) {
+      goodGuessArray.push(letter.toUpperCase());
+    } else {
+      goodGuessArray.push("●");
+    }
+  }
+  inProgress.innerText = goodGuessArray.join("");
+  gameWon();
+};
+
+const gameWon = function() {
+  if (word.toUpperCase() === inProgress.innerText) {
+    message.classList.add("win");
+    message.innerHTML = `<p class="highlight">You guessed the correct word! Congrats!</p>`
+  }
 };
